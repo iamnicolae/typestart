988ff584965efe2963e545e136875129b2ebdad4
Nicolae <andronicnicolae15@gmail.com>
2023-09-07 11:03:45 +0300

    lowered the added time when correctly typed

--- scripts/utils/setTime.js
+++ scripts/utils/setTime.js
@@ -1,6 +1,6 @@
 import { time } from "../src/time.js";
 
 export function setTime() {
-  time.set = time.get + 1000;
-  time.setTotal = 1000;
+  time.set = time.get + 500;
+  time.setTotal = 500;
 }
