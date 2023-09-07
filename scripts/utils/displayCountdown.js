be6eb59960ee09c67b120ccc5183816b3dea685c
Nicolae <andronicnicolae15@gmail.com>
2023-09-07 11:08:41 +0300

    fixed countdown timer leading 0

--- scripts/utils/displayCountdown.js
+++ scripts/utils/displayCountdown.js
@@ -3,6 +3,7 @@ import { countdownTimer } from "./querySelectors.js";
 export function displayCountdown(time) {
   let seconds = Math.floor(time / 1000).toFixed(0);
   let milliseconds = ((time / 1000) - Math.floor(time / 1000)).toFixed(2);
+  const leadingZero = seconds > 9;
 
-  countdownTimer.innerHTML = `0${seconds}:${(milliseconds + "").split(".")[1]}s`;
+  countdownTimer.innerHTML = `${leadingZero ? "" : "0"}${seconds}:${(milliseconds + "").split(".")[1]}s`;
 }
